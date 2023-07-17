266f345d0d3077e38a188173ed2ae1de4eb06372
fkanedev <fkane.dev@gmail.com>
2023-07-17 15:56:42 -0400

    Part C Task 3 first part DONE
    Signed-off-by: fkanedev <fkane.dev@gmail.com>

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -1,14 +1,30 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext'; 
 const Currency = () => {
-    const { expenses, budget } = useContext(AppContext);
-    const totalExpenses = expenses.reduce((total, item) => {
-        return (total = total + item.cost);
-    }, 0);
-    const alertType = totalExpenses > budget ? 'alert-danger' : 'alert-success';
+    const { expenses, currency } = useContext(AppContext);
+    const [name, setName] = useState("");
+    
+    const updateCurrency = (newCurrency) => {
+        
+        dispatch({
+            type: 'CHG_CURRENCY',
+            payload: newCurrency
+        });
+    }
+
     return (
-        <div className={`alert ${alertType}`}>
-            <span>Remaining: £{budget - totalExpenses}</span>
+        <div className='alert alert-secondary'>
+            <div class="dropdown">
+            <button  class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">
+                <span id="curr" >Currency({currency}) </span>
+            <span class="caret"></span></button>
+            <ul class="dropdown-menu" onclick={(event) => updateCurrency(event.target.value)}>
+                <li name="$ Dollar" value="$" >header</li>
+                <li name="£ Pound" value="£" >header</li>
+                <li name="€ Euro" value="€" >header</li>
+                <li name="₹ Ruppee" value="₹" >header</li>
+            </ul>
+            </div>
         </div>
     );
 };
