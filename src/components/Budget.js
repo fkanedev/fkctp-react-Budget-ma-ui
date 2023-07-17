e7bed679484678d2587b8460ee3a2fb30212e0ff
fkanedev <fkane.dev@gmail.com>
2023-07-17 14:58:02 -0400

    Part C Task 2all parts DONE
    Signed-off-by: fkanedev <fkane.dev@gmail.com>

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,8 +1,23 @@
 import React, { useContext, /*useState*/ } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
-    const {dispatch, budget } = useContext(AppContext);
+    const {dispatch, budget, expenses } = useContext(AppContext);
+    const upper_limit_budget = 20000;
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total = total + item.cost);
+    }, 0);
     const updateBudget = (newBudget) => {
+
+        if(newBudget < totalExpenses) {
+            alert("You cannot reduce the budget value lower than the spending");
+            return;
+        }
+
+        if(newBudget > upper_limit_budget) {
+            alert("The value cannot exceed remaining funds  £"+upper_limit_budget);
+            newBudget = budget;
+        }
+
         dispatch({
             type: 'SET_BUDGET',
             payload: parseInt(newBudget)
